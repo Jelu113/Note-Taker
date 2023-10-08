a8c7c057538ff843839d474ebe6ddafc43bdde1f
jelu <jenaehe@yahoo.com>
2023-10-08 12:37:11 -0600

    Updating to try and get Insomnia error corrected.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+// const express = require('express');
+// const fs = require('fs');
+// // Import our modular routers for /tips and /feedback
+//  const dbrouter = require('./db/db.json');
+// // const feedbackRouter = require('./feedback');
+// // const diagnosticsRouter = require('./diagnostics');
 
-// Import our modular routers for /tips and /feedback
- const dbrouter = require('./db/db.json');
-// const feedbackRouter = require('./feedback');
-// const diagnosticsRouter = require('./diagnostics');
+// const app = express();
 
-const app = express();
+// app.use(dbrouter);
+// // app.use('/feedback', feedbackRouter);
+// // app.use('/diagnostics', diagnosticsRouter);
 
-app.use(dbrouter, title);
-// app.use('/feedback', feedbackRouter);
-// app.use('/diagnostics', diagnosticsRouter);
-
-module.exports = app;
+// module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require("express")
 const path = require('path');
-const fs= require("fs");
-const dbroutes = require("./db/db.json")
-
+// const fs = require("fs");
+// const dbroutes = require("./db/db.json")
 
 const PORT = 3001;
 
 const app = express();
 
-app.get("/api", (req, res)=> res.json(dbroutes));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.fs());
 
-    app.use(express.static(path.join(__dirname, "public")));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
-app.listen(PORT,()=>
-console.log(`App listening at http://localhost:${PORT}`)
+app.get('*',(req,res) => {
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+// app.post('/db/db.json',(req, res)=> {
+//     console.info(`${req.method} request was received`);
+//     let response;
+
+//     if (req.body && req.body.product) {
+//         response ={
+//             status:'success',
+//             data: req.body,
+//         };
+//         res.json (`${response.data.product} has been added`);
+//     }else{
+//         res.json('Request not complete.')
+//     }
+// });
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
 );
