723121514c8fd5e93c038dd48cf0a8c7e39f34e6
jelu <jenaehe@yahoo.com>
2023-10-08 21:12:59 -0600

    fixed a typo on the index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
-
 const express = require('express');
 
 // Import our files containing our routes
-const title = require('./notes');
+const notesRouter = require('./notes');
 
 // Create and instance of express so we can apply the middleware and routing
 const app = express();
 
-
-app.use('/notes', notes);
-
-
+app.use('/notes', notesRouter);
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -4,27 +4,27 @@ const uuid = require('../helpers/uuid');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) => {
-    console.info(`${req.method} request received for note`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+  console.info(`${req.method} request received for note`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-  notes.post('/', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-  
-    const { title, text, note } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        note_id: uuid(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(`Note added successfully 🚀`);
-    } else {
-      res.error('Error in adding Note');
-    }
-  });
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
 
-  module.exports = notes;
+  const { title, text, note } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+module.exports = notes;
